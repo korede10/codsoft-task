e34faaa587758dca6a32118222e04f0752053876
faiz <jamiuabdulrahmon100@gmail.c0m>
2025-04-19 22:05:57 +0100

    first commit

---
+++ portfolio/portfolio.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector("nav");
+
+    menuToggle.addEventListener("click", () => {
+        nav.classList.toggle("open");
+    });
+});
